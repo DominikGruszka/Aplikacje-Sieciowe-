574ce919e5d8b5c63b98a404fd734f27f53eed94
DominikGruszka <dominikgruszka@poczta.onet.pl>
2024-11-16 13:24:31 +0100

    Add files via upload

---
+++ app/templates_c/0c204205d5cc3ca37196a1d3cec037e1663a6082_0.file_calc.html.php
@@ -0,0 +1,130 @@
+<?php
+/* Smarty version 5.4.1, created on 2024-11-06 20:53:09
+  from 'file:C:\xampp\htdocs\php_04_uproszczony/app/calc.html' */
+
+/* @var \Smarty\Template $_smarty_tpl */
+if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
+  'version' => '5.4.1',
+  'unifunc' => 'content_672bc925e12fd3_61124638',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '0c204205d5cc3ca37196a1d3cec037e1663a6082' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\zadanie3/app/kalkulator.html',
+      1 => 1730922622,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+))) {
+function content_672bc925e12fd3_61124638 (\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\zadanie3\\app';
+$_smarty_tpl->getInheritance()->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_1632602690672bc925dc0f42_35357458', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_1010097105672bc925dccac5_47762849', 'content');
+$_smarty_tpl->getInheritance()->endChild($_smarty_tpl, "../templates/main.html", $_smarty_current_dir);
+}
+/* {block 'footer'} */
+class Block_1632602690672bc925dc0f42_35357458 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\zadanie3\\app';
+?>
+przykładowa tresć stopki wpisana do szablonu głównego z szablonu kalkulatora<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_1010097105672bc925dccac5_47762849 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\zadanie3\\app';
+?>
+
+
+<h3>Kalkulator kredytowy</h2>
+
+
+<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->getValue('app_url');?>
+/app/kalkulator.php" method="post">
+	<fieldset>
+		<label for="Kwota_kredytu">Kwota kredytu</label>
+		<input id="kwota_kredytu" type="text" placeholder="wartość kwota_kredytu" name="kwota_kredytu" value="{$form['kwota_kredytu']}">
+		
+		<label for="lata_splaty">Lata spłaty kredytu</label>
+		<input id="lata_splaty" type="text" placeholder="wartość lata_splaty" name="lata_splaty" value="{$form['lata_splaty']}">
+		
+		<label for="oprocentowanie">Aktualne oprocentowanie</label>
+		<input id="oprocentowanie" type="text" placeholder="wartość oprocentowanie" name="oprocentowanie" value="{$form['oprocentowanie']}">
+		
+">
+	</fieldset>
+	<button type="submit" class="pure-button pure-button-primary">Oblicz</button>
+</form>
+
+<div class="messages">
+
+<?php if ((null !== ($_smarty_tpl->getValue('messages') ?? null))) {?>
+	<?php if ($_smarty_tpl->getSmarty()->getModifierCallback('count')($_smarty_tpl->getValue('messages')) > 0) {?> 
+		<h4>Wystąpiły błędy: </h4>
+		<ol class="err">
+		<?php
+$_from = $_smarty_tpl->getSmarty()->getRuntime('Foreach')->init($_smarty_tpl, $_smarty_tpl->getValue('messages'), 'msg');
+$foreach0DoElse = true;
+foreach ($_from ?? [] as $_smarty_tpl->getVariable('msg')->value) {
+$foreach0DoElse = false;
+?>
+		<li><?php echo $_smarty_tpl->getValue('msg');?>
+</li>
+		<?php
+}
+$_smarty_tpl->getSmarty()->getRuntime('Foreach')->restore($_smarty_tpl, 1);?>
+		</ol>
+	<?php }
+}?>
+
+<?php if ((null !== ($_smarty_tpl->getValue('infos') ?? null))) {?>
+	<?php if ($_smarty_tpl->getSmarty()->getModifierCallback('count')($_smarty_tpl->getValue('infos')) > 0) {?> 
+		<h4>Informacje: </h4>
+		<ol class="inf">
+		<?php
+$_from = $_smarty_tpl->getSmarty()->getRuntime('Foreach')->init($_smarty_tpl, $_smarty_tpl->getValue('infos'), 'msg');
+$foreach1DoElse = true;
+foreach ($_from ?? [] as $_smarty_tpl->getVariable('msg')->value) {
+$foreach1DoElse = false;
+?>
+		<li><?php echo $_smarty_tpl->getValue('msg');?>
+</li>
+		<?php
+}
+$_smarty_tpl->getSmarty()->getRuntime('Foreach')->restore($_smarty_tpl, 1);?>
+		</ol>
+	<?php }
+}?>
+
+<?php if ((null !== ($_smarty_tpl->getValue('wynik') ?? null))) {?>
+	<h4>Wynik</h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->getValue('wynik');?>
+
+	</p>
+<?php }?>
+
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
