554224dc2230b0462f76b3de087effa28da0a931
DominikGruszka <dominikgruszka@poczta.onet.pl>
2024-12-09 21:33:07 +0100

    Add files via upload

---
+++ zadanie_6a/templates_c/1b8d20bd5f38bb3a179700254c4924dca3a9046a_0.file.messages.tpl.php
@@ -0,0 +1,63 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-12-08 19:43:48
+  from "C:\xampp\htdocs\zadanie_6a\app\views\templates\messages.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_6755e8e4a05f70_30660297',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '1b8d20bd5f38bb3a179700254c4924dca3a9046a' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\zadanie_6a\\app\\views\\templates\\messages.tpl',
+      1 => 1733683129,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6755e8e4a05f70_30660297 (Smarty_Internal_Template $_smarty_tpl) {
+if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+<div class="messages error">
+	<ol>
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'err');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+</div>
+<?php }
+if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+<div class="messages info bottom-margin">
+	<ol>
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'inf');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+</div>
+<?php }
+}
+}
---
+++ zadanie_6a/templates_c/3b4e19d16482d872f25dffa134e54e19075454f3_0.file.LoginView.tpl.php
@@ -0,0 +1,70 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-12-08 19:43:48
+  from "C:\xampp\htdocs\zadanie_6a\app\views\LoginView.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_6755e8e4635673_87907558',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '3b4e19d16482d872f25dffa134e54e19075454f3' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\zadanie_6a\\app\\views\\LoginView.tpl',
+      1 => 1733683129,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:main.tpl' => 1,
+    'file:messages.tpl' => 1,
+  ),
+),false)) {
+function content_6755e8e4635673_87907558 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_3925507986755e8e4621df9_56581020', 'content');
+?>
+
+<?php $_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'content'} */
+class Block_3925507986755e8e4621df9_56581020 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+<form action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+login" method="post"  class="pure-form pure-form-aligned bottom-margin">
+	<legend>Logowanie do systemu</legend>
+	<fieldset>
+        <div class="pure-control-group">
+			<label for="id_login">login: </label>
+			<input id="id_login" type="text" name="login"/>
+		</div>
+        <div class="pure-control-group">
+			<label for="id_pass">pass: </label>
+			<input id="id_pass" type="password" name="pass" /><br />
+		</div>
+		<div class="pure-controls">
+			<input type="submit" value="zaloguj" class="pure-button pure-button-primary"/>
+		</div>
+	</fieldset>
+</form>	
+
+<?php $_smarty_tpl->_subTemplateRender("file:messages.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
